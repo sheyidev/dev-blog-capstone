d861b020b7e77b497f343e5802d74c017c34e8ee
Sheyi <seezie@gmail.com>
2024-10-23 04:09:37 -0500

    all allowed host

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure--4iws!uc#2j2cwb2llj!)k&lw3=+zy^2cxqrdj2h=nai8eiq*8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost', '127.0.0.1').split(',')
+#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost', '127.0.0.1').split(',')
 
 
 # Application definition
